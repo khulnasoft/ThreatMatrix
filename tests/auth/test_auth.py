85780e05377b05c74beee3651653351656e7d1fe
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-11-12 04:22:44 +0000

    update django-iam-dbauth

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -290,7 +290,7 @@ class TestUserAuth(CustomOAuthTestCase):
             "username": "blahblah",
             "first_name": "blahblah",
             "last_name": "blahblah",
-            "password": "threatmatrixthreatmatrix$",
+            "password": "trmatrixtrmatrix$",
         }
 
         response = self.client.post(register_uri, body)
